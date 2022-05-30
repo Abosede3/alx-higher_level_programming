e5f869e8abd54396db8b8902c436e68b7c79054b
Abosede3 <66247750+Abosede3@users.noreply.github.com>
2022-05-30 23:54:41 +0100

    Create  7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {0:2}")
+print(f"Last 2 letters: {7:8}")
+print(f"Middle word: {3:-4}")
